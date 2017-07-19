1ef2aecc8fa158d82cd808ea363668a847bd1d63
hands <hands@email.arizona.edu>
2017-07-19 11:16:51 -0700

    Added e310 probe

---
+++ python/gps_probe_e310.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2017 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+from gnuradio import gr
+import pmt
+import subprocess
+import tempfile
+
+class gps_probe_e310(gr.sync_block):
+    """
+    docstring for block gps_probe_e310
+    """
+    def __init__(self, parent, target):
+        gr.sync_block.__init__(self,
+            name="gps_probe_e310",
+            in_sig=[],
+            out_sig=[])
+        self.message_port_register_in(pmt.intern("pdus"))
+        self.message_port_register_out(pmt.intern("pdus"))
+        self.set_msg_handler(pmt.intern("pdus"), self.handler)
+        (self.gps_log, self.gps_log_path) = tempfile.mkstemp()
+        subprocess.Popen("gpspipe -r -o " + self.gps_log_path  + " -t | grep GPGGA", shell=True)
+
+    def __del__(self):
+        close(self.gps_log)
+        # delete the log for security and space
+
+    def work(self, input_items, output_items):
+        assert(False)
+
+    def handler(self pdu):
+       (ometa, data) = (pmt.to_python(pmt.car(pdu)), pmt.cdr(pdu))
+
+       d = {}
+       try:
+            # grab all mboard sensor data
+            uhd_source = eval("self.parent.%s"%(self.target))
+            mbs = uhd_source.get_mboard_sensor_names()
+            for k in mbs:
+                v = uhd_source.get_mboard_sensor(k)
+                d[k] = v.value
+            d["gain"] = uhd_source.get_gain()
+            d["gps_present"] = True
+            d["gps_location"] = str(self.gps_log.readlines()[-1])
+        except AttributeError:
+            d["gps_present"] = False
+        
+        ometa.update( d )
+        self.message_port_pub(pmt.intern("pdus"), pmt.cons(pmt.to_pmt(ometa), data))
+
+
