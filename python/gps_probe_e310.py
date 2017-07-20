902bf0f28dddbfd7365092b69a09f29e6d089daa
hands <hands@email.arizona.edu>
2017-07-19 18:39:01 -0700

    y u no work

--- python/gps_probe_e310.py
+++ python/gps_probe_e310.py
@@ -19,34 +19,106 @@
 # Boston, MA 02110-1301, USA.
 # 
 
-from gnuradio import gr
-import pmt
-import subprocess
-import time
-import tempfile
-from gps3 import gps3
-#from gps3 import agps3
+#from gnuradio import gr
+#import pmt
+#import subprocess
+#import time
+#import tempfile
+#from gps3 import gps3
+##from gps3 import agps3
+#
+#class gps_probe_e310(gr.sync_block):
+#    """
+#    docstring for block gps_probe_e310
+#    """
+#    def __init__(self, parent, target):
+#        gr.sync_block.__init__(self,
+#            name="gps_probe_e310",
+#            in_sig=[],
+#            out_sig=[])
+#        self.parent = parent
+#        self.target = target
+#        self.message_port_register_in(pmt.intern("pdus"))
+#        self.message_port_register_out(pmt.intern("pdus"))
+#        self.set_msg_handler(pmt.intern("pdus"), self.handler)
+#       # (self.gps_log, self.gps_log_path) = tempfile.mkstemp()
+#       # subprocess.Popen("gpspipe -r -o -d " + self.gps_log_path  + " -t | grep GPGGA", shell=True)
+#
+#    #def __del__(self):
+#    #    close(self.gps_log)
+#    #    # delete the log for security and space
+#
+#    def work(self, input_items, output_items):
+#        assert(False)
+#
+#    def handler(self, pdu):
+#        (ometa, data) = (pmt.to_python(pmt.car(pdu)), pmt.cdr(pdu))
+#
+#        d = {}
+#        try:
+#            # grab all mboard sensor data
+#            uhd_source = eval("self.parent.%s"%(self.target))
+#            mbs = uhd_source.get_mboard_sensor_names()
+#            for k in mbs:
+#                v = uhd_source.get_mboard_sensor(k)
+#                d[k] = v.value
+#            #d["gps_location"] = str(self.gps_log.readlines()[-1])
+#	   # d["gps_location"] = str(subprocess.check_output['tail','-n','1', self.gps_log_path])
+#            d["gain"] = uhd_source.get_gain()
+#            d["gps_present"] = True
+#
+#            print "I am normal"
+#
+#    #       gps_socket = agps3.GPSDSocket()
+#    #       data_stream = agps3.DataStream()
+#    #       gps_socket.connect()
+#    #       gps_socket.watch()
+#    #       for new_data in gps_socket:
+#    #           print "new data exists?"
+#    #           if new_data:
+#    #               print "hullo thar"
+#    #               data_stream.unpack(new_data)
+#    #               print 'Latitude = ', data_stream.lat
+#
+#        except AttributeError:
+#            d["gps_present"] = False
+#
+#        ometa.update( d )
+#        self.message_port_pub(pmt.intern("pdus"), pmt.cons(pmt.to_pmt(ometa), data))
 
-class gps_probe_e310(gr.sync_block):
-    """
-    docstring for block gps_probe_e310
-    """
+
+#!/usr/bin/env python
+#
+#  Copyright 2015 Tim O'Shea
+#
+#  This file is part of gr-uhdgps.
+#
+#  gr-uhdgps is free software: you can redistribute it and/or modify it under the terms of the
+#  GNU General Public License as published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  gr-uhdgps is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  See the GNU General Public License for more details. You should have received a copy of the GNU
+#  General Public License along with gr-uhdgps. If not, see <http://www.gnu.org/licenses/>.
+#
+from gnuradio import gr
+import pprint;
+import pmt
+import time;
+
+class gps_probe(gr.sync_block):
     def __init__(self, parent, target):
         gr.sync_block.__init__(self,
-            name="gps_probe_e310",
-            in_sig=[],
-            out_sig=[])
+                name = "pdu_timestamp",
+                in_sig = [],
+                out_sig = []);
         self.parent = parent
         self.target = target
-        self.message_port_register_in(pmt.intern("pdus"))
-        self.message_port_register_out(pmt.intern("pdus"))
-        self.set_msg_handler(pmt.intern("pdus"), self.handler)
-       # (self.gps_log, self.gps_log_path) = tempfile.mkstemp()
-       # subprocess.Popen("gpspipe -r -o -d " + self.gps_log_path  + " -t | grep GPGGA", shell=True)
-
-    #def __del__(self):
-    #    close(self.gps_log)
-    #    # delete the log for security and space
+        self.message_port_register_in(pmt.intern("pdus"));
+        self.message_port_register_out(pmt.intern("pdus"));
+        self.set_msg_handler(pmt.intern("pdus"), self.handler);
 
     def work(self, input_items, output_items):
         assert(False)
@@ -54,36 +126,20 @@ class gps_probe_e310(gr.sync_block):
     def handler(self, pdu):
         (ometa, data) = (pmt.to_python(pmt.car(pdu)), pmt.cdr(pdu))
 
-        d = {}
+        d = {};
         try:
             # grab all mboard sensor data
-            uhd_source = eval("self.parent.%s"%(self.target))
-            mbs = uhd_source.get_mboard_sensor_names()
+            uhd_source = eval("self.parent.%s"%(self.target));
+            mbs = uhd_source.get_mboard_sensor_names();
             for k in mbs:
-                v = uhd_source.get_mboard_sensor(k)
+                v = uhd_source.get_mboard_sensor(k);
                 d[k] = v.value
-            #d["gps_location"] = str(self.gps_log.readlines()[-1])
-	   # d["gps_location"] = str(subprocess.check_output['tail','-n','1', self.gps_log_path])
-            d["gain"] = uhd_source.get_gain()
+            d["gain"] = uhd_source.get_gain();
             d["gps_present"] = True
-
-            print "I am normal"
-
-    #       gps_socket = agps3.GPSDSocket()
-    #       data_stream = agps3.DataStream()
-    #       gps_socket.connect()
-    #       gps_socket.watch()
-    #       for new_data in gps_socket:
-    #           print "new data exists?"
-    #           if new_data:
-    #               print "hullo thar"
-    #               data_stream.unpack(new_data)
-    #               print 'Latitude = ', data_stream.lat
-    
         except AttributeError:
             d["gps_present"] = False
 
-        ometa.update( d )
-        self.message_port_pub(pmt.intern("pdus"), pmt.cons(pmt.to_pmt(ometa), data))
-
+        #pprint.pprint(d);
+        ometa.update( d );
+        self.message_port_pub(pmt.intern("pdus"), pmt.cons(pmt.to_pmt(ometa), data));
 
