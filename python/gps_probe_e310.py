abdde40b1186a1c4099e87a1d8b01743e660cd90
hands <hands@email.arizona.edu>
2017-07-19 17:51:09 -0700

    Plz work

--- python/gps_probe_e310.py
+++ python/gps_probe_e310.py
@@ -66,14 +66,14 @@ class gps_probe_e310(gr.sync_block):
             d["gain"] = uhd_source.get_gain()
             d["gps_present"] = True
 
-	    gps_socket = gps3.GPSDSocket()
-	    data_stream = gps3.DataStream()
-	    gps_socket.connect()
-	    gps_socket.watch()
-	    for new_data in gps_socket:
-	        if new_data:
-		    data_stream.unpack(new_data)
-		    d["gps_location"] = data_stream.TPV['lat']
+            gps_socket = gps3.GPSDSocket()
+            data_stream = gps3.DataStream()
+            gps_socket.connect()
+            gps_socket.watch()
+            for new_data in gps_socket:
+                if new_data:
+                    data_stream.unpack(new_data)
+                    print 'Latitude = ', data_stream.TPV['lat']
             
         except AttributeError:
             d["gps_present"] = False
